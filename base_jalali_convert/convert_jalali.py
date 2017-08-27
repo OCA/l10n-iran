1f5c2d90abb71abb9166416ade4924f8e4948acd
elicoidal <eric.caudal@elico-corp.com>
2017-08-27 14:26:23 +0800

    Fixed travis

--- base_jalali_convert/convert_jalali.py
+++ base_jalali_convert/convert_jalali.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 import datetime
 import logging
+_logger = logging.getLogger(__name__)
 try:
     import jdatetime
 except (ImportError, IOError) as err:
