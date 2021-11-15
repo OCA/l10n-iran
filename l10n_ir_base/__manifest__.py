2f5bb7b21ef9ee9734e578350e364d617c8fe132
saeed <38601142+saeed-raeisi@users.noreply.github.com>
2021-11-15 12:40:01 +0330

    Update manifest -> category
    add Localization/Iran

--- l10n_ir_base/__manifest__.py
+++ l10n_ir_base/__manifest__.py
@@ -7,7 +7,7 @@
     "maintainer": ["saeed-raesi"],
     "website": "https://github.com/OCA/l10n-iran",
     "license": "AGPL-3",
-    "category": "l10n/Technical",
+    "category": "Localization/Iran",
     "summary": "Iran Base Calendar",
     "depends": ["base", "account"],
     "data": ["views/user_preferences.xml", "views/partner_preferences.xml"],
