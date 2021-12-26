9b4e743bc80725a75f42ad81a58d17aa17e43f2a
mrbayati2002 <mrbayati2002@gmail.com>
2021-12-26 14:42:52 +0330

    fix some typos in README and model

--- l10n_ir_base/models/user_extensions.py
+++ l10n_ir_base/models/user_extensions.py
@@ -18,5 +18,5 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     l10n_ir_national_code = fields.Char(
-        string="Natinal Code", size=11, help="National code used for iran."
+        string="National Code", size=11, help="National code used for iran."
     )
