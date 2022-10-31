aa4300698104339be28fc83248216aaec911500f
Saeed Raeisi <saeed.raesi2020@gmail.com>
2022-10-31 13:26:04 +0330

    [MIG] 14->16 l10n_ir

---
+++ setup/_metapackage/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo14-addons-oca-l10n-iran",
+    description="Meta package for oca-l10n-iran Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo14-addon-l10n_ir_accounting',
+        'odoo14-addon-l10n_ir_hr_contract',
+        'odoo14-addon-l10n_ir_states',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 14.0',
+    ]
+)
