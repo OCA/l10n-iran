7846ce5fe988c11ea16c6db715c8ad33b66412ce
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2024-05-01 20:45:31 +0200

    [FIX] l10n_ir_account: Pre-commit fixes

--- l10n_ir_account/__manifest__.py
+++ l10n_ir_account/__manifest__.py
@@ -1,10 +1,10 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "Iran - Accounting",
     "version": "17.0.1.0.0",
     "countries": ["ir"],
+    "author": "Fadoo,Odoo Community Association (OCA)",
     "category": "Accounting/Localizations/Account Charts",
     "summary": """iran accounting chart and localization.""",
     "license": "AGPL-3",
