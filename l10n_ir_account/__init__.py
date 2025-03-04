4958e8fc053832728074fca07286a2e46434c92b
Ali Sharifi <alisharifi997as@gmail.com>
2025-03-04 09:20:32 +0330

    [MIG] l10n_ir_account: Migration to 18.0

---
+++ l10n_ir_account/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2024-Today: Odoo Community Iran
+# @author: Odoo Community Iran (https://odoo-community.ir/
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from . import models
---
+++ l10n_ir_account/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2024-Today: Odoo Community Iran
+# @author: Odoo Community Iran (https://odoo-community.ir/
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Iran - Accounting",
+    "version": "1.0.1",
+    "countries": ["ir"],
+    "category": "Accounting/Localizations/Account Charts",
+    "summary": """iran accounting chart and localization.""",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/l10n-iran",
+    "depends": ["account"],
+    "data": [
+        "data/res_currency_data.xml",
+        "data/res.bank.csv",
+    ],
+}
